7fbfbdd399cca322554a2f472430a8af82004d72
Alejololer <carlos.aleman@epn.edu.ec>
2024-02-25 23:45:04 -0500

    Añadidas las capas de bases de datos, trabajando en actualizar el Framework de PlayerUI

---
+++ DataAccess/UserDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class UserDAO : ConnectionToSQL
+    {
+        public bool Login(string username, string password)
+        {
+            using(var connection = GetConnection())
+            {
+                connection.Open ();
+                using (var command = new SqlCommand ()) {
+                    command.Connection = connection;
+                    command.CommandText = "Select * from Users where nombreUser=@user and claveUser=@password";
+                    command.Parameters.AddWithValue ("@user", username);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.CommandType = System.Data.CommandType.Text;
+                    SqlDataReader reader = command.ExecuteReader ();
+                    if(reader.HasRows)
+                    {
+                        return true;
+                    }else
+                        return false;
+                }
+            }
+        }
+
+    }
+}
---
+++ Domain/UserModel.cs
@@ -0,0 +1,15 @@
+﻿
+using DataAccess;
+
+namespace Domain
+{
+    public class UserModel
+    {
+        UserDAO userDAO = new UserDAO();
+        public bool LoginUser(string username, string password)
+        {
+            return userDAO.Login(username, password);
+        }
+
+    }
+}
