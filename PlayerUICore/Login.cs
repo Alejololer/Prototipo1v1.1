0314051ea40be1fac81e3c87e6e614a8801f5425
Alejololer <carlos.aleman@epn.edu.ec>
2024-02-26 00:43:21 -0500

    Conseguida la transformación a creo .NET 8.0? Ni idea, pero ya vale la wea fome, top 10 weas fomes ayuda con pedidos me qquiero mtar

---
+++ PlayerUICore/Login.cs
@@ -0,0 +1,87 @@
+﻿using PlayerUI.ConeccionBD;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Domain;
+
+
+namespace PlayerUI
+{
+    public partial class Login : Form
+    {
+
+        // Conexión a la base de datos
+        private const string connectionString = "Data Source=TONY;Initial Catalog=Laboratorio;Integrated Security=True;Encrypt=False"; // Reemplaza con tu cadena de conexión
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text != "")
+            {
+                if (txtContrasena.Text != "")
+                {
+                    UserModel user = new UserModel();
+                    var validLogin = user.LoginUser(txtUsuario.Text, txtContrasena.Text);
+                    if (validLogin == true)
+                    {
+                        this.Hide();
+
+                        // Crear una instancia del nuevo formulario que quieres mostrar
+                        Menu nuevoFormulario = new Menu();
+
+                        // Suscribir al evento FormClosed del nuevo formulario
+                        nuevoFormulario.FormClosed += NuevoFormulario_FormClosed;
+
+                        // Mostrar el nuevo formulario
+                        nuevoFormulario.Show();
+                    }
+                    else
+                    {
+                        msgError("Nombre de usuario o contraseña incorrectos. Intente de nuevo");
+                        txtContrasena.Clear();
+                        txtUsuario.Clear(); 
+                    } 
+                }
+                else msgError("Ingrese la contraseña");
+            }
+            else msgError("Ingrese el nombre de usuario");
+            
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Método para manejar el evento FormClosed del nuevo formulario
+        private void NuevoFormulario_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Mostrar nuevamente el formulario original una vez que se cierre el segundo formulario
+            this.Show();
+        }
+
+        private void msgError(string msg)
+        {
+            lblError.Text = msg;
+            lblError.Visible = true;
+        }
+    }
+}
---
+++ PlayerUICore/Pacientes/RegistrarPaciente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayerUI.Pacientes
+{
+    public partial class RegistrarPaciente : Form
+    {
+        public RegistrarPaciente()
+        {
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Enviar un mensaje
+            DialogResult result = MessageBox.Show("¿Está seguro?", "Registro de Paciente", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("Paciente registrado con éxito", "Registro de Paciente", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+            }
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
